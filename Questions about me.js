168927a80d875d0b970dd7fda9f001ebd60e7f9e
christol <christol.beresford@hotmail.co.uk>
2023-06-13 14:54:53 +0100

    initial commit

---
+++ Questions about me.js
@@ -0,0 +1,15 @@
+// All questions are Yes or No. or Y/N. Use .toUpperCase
+
+let userName = prompt("What is your name?");
+let userNameReply = alert("Hi " + userName);
+let userFeelings = prompt("How are you feeling today?");
+let userFeelingsReply = alert("Yeah, feel" + userFeelings sometimes too")
+let userAge = prompt("How old are you?");
+let userChoice = prompt("Would you like to know a bit about me?")
+
+let MyFavouriteColour = "Orange"
+console.log(MyFavouriteColour)
+
+
+//missing - add alerts to give response to user.
+//add alert "response to user input" check chat. 
---
+++ app.js
@@ -0,0 +1,9 @@
+// All questions are Yes or No. or Y/N. Use .toUpperCase
+
+let userName = prompt("What is your name?");
+let userFeelings = prompt("How are you feeling today?")
+let userAge = prompt("How old are you?");
+let userChoice = prompt("Would you like to know a bit about me?")
+
+let MyFavouriteColour = "Orange"
+console.log(MyFavouriteColour)
